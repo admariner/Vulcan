ee4a1d39b592e79c18d12e0891f945ecb370fb6d
Sacha Greif <git@sachagreif.com>
2024-10-04 14:01:46 +0900

    add local copy of fourseven:scss package (see https://github.com/Meteor-Community-Packages/meteor-scss/issues/314#issuecomment-2291897856 )

---
+++ packages/meteor-scss/package.js
@@ -0,0 +1,51 @@
+Package.describe({
+  summary: "Style with attitude. Sass and SCSS support for Meteor.js.",
+  version: "4.17.0-rc.0",
+  git: "https://github.com/Meteor-Community-Packages/meteor-scss.git",
+  name: "fourseven:scss",
+});
+
+Package.registerBuildPlugin({
+  name: "compileScssBatch",
+  use: ["caching-compiler@1.2.2", "ecmascript@0.16.2"],
+  sources: ["plugin/compile-scss.js"],
+  npmDependencies: {
+    sass: "1.77.8",
+    "@babel/runtime": "7.24.5",
+  },
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom(["2.8.0", "3.0.1"]);
+  api.use("isobuild:compiler-plugin@1.0.0");
+});
+
+Package.onTest(function (api) {
+  api.versionsFrom(["2.8.0", "3.0.1"]);
+  api.use(["test-helpers", "tinytest"]);
+
+  api.use(["fourseven:scss"]);
+
+  // Tests for .scss
+  api.addFiles([
+    "test/scss/_emptyimport.scss",
+    "test/scss/_not-included.scss",
+    "test/scss/_top.scss",
+    "test/scss/_top3.scss",
+    "test/scss/empty.scss",
+    "test/scss/dir/_in-dir.scss",
+    "test/scss/dir/_in-dir2.scss",
+    "test/scss/dir/root.scss",
+    "test/scss/dir/subdir/_in-subdir.scss",
+  ]);
+
+  api.addFiles("test/scss/top2.scss", "client", { isImport: true });
+
+  // Test for includePaths
+  api.addFiles([
+    "test/include-paths/include-paths.scss",
+    "test/include-paths/modules/module/_module.scss",
+  ]);
+
+  api.mainModule("tests.js", "client");
+});
